32d259d801437025cc9f0ee2a3086ad2169f3d6b
Martin Riedl <martin.riedl.92@googlemail.com>
2017-04-30 16:14:15 +0200

    add yasm to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 # set some environment variables
+echo "use FF_ROOT = $FF_ROOT"
 export FF_SOURCE="$FF_ROOT/ffsource"
 export FF_OUT="$FF_ROOT/ffout"
 
+# download and install yasm
+echo "start downloading yasm..."
+export FF_SOURCE_YASM="$FF_SOURCE/yasm"
+export FF_OUT_YASM="$FF_OUT/yasm"
+mkdir "$FF_SOURCE_YASM"
+cd "$FF_SOURCE_YASM"
+curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar -zxf yasm-1.3.0.tar.gz
+./configure --prefix="$FF_OUT_YASM"
+make
+make install
+export PATH="$FF_OUT_YASM/bin:$PATH"
+
 # download ffmpeg
+echo "start downloading ffmpeg..."
 mkdir "$FF_SOURCE"
 cd "$FF_SOURCE"
 curl -O https://ffmpeg.org/releases/ffmpeg-3.3.tar.gz
@@ -10,6 +25,7 @@ tar -zxf ffmpeg-3.3.tar.gz
 export FFMPEG_SOURCE="$FF_SOURCE/ffmpeg-3.3"
 
 # build ffmpeg
+echo "start build process..."
 cd "$FFMPEG_SOURCE"
 ./configure --prefix="$FF_OUT"
 make
