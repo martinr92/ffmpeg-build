0d6cf026dd4b25b38a89e426fd135377c9ea6fae
Martin Riedl <martin.riedl.92@googlemail.com>
2018-05-02 19:48:51 +0200

    cmake downgrade to 3.10.3

--- build.sh
+++ build.sh
@@ -102,12 +102,12 @@ endBlock nasm
 
 # download cmake
 startBlock cmake
-export FF_CMAKE_VERSION=3.11.1
+export FF_CMAKE_VERSION=3.10.3
 export FF_OUT_CMAKE="$FF_ROOT/cmake/cmake-${FF_CMAKE_VERSION}"
 if [ ! -f $FF_OUT_CMAKE/bin/cmake ]
 then
     cd "$FF_SOURCE"
-    curl -O https://cmake.org/files/v3.11/cmake-${FF_CMAKE_VERSION}.tar.gz
+    curl -O https://cmake.org/files/v3.10/cmake-${FF_CMAKE_VERSION}.tar.gz
     checkExecutionStatus "download of cmake" $?
 
     # build cmake
